168125c9cdeab3a8e6d660df3ddb70f282b89d1c
Stenio Wagner <stenio.wagner1@gmail.com>
2020-03-09 22:00:20 -0300

    feat: installing apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Platform } from 'react-native';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 import { ThemeContextProvider } from './styles/theme-context-provider/ThemeContextProvider';
 import AndroidNavigationBar from './components/AndroidNavigationBar';
 import Navigation from './routes/Routes';
 
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
+
 const App = () => (
   <ThemeContextProvider>
     <>
-      <Navigation />
+      <ApolloProvider
+        client={client}
+      >
+        <Navigation />
+      </ApolloProvider>
       {Platform.OS === 'android' && <AndroidNavigationBar />}
     </>
   </ThemeContextProvider>
