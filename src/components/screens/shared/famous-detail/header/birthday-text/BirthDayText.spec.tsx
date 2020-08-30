451519c02d1f52bfe22ad1b6fc2e71cb71e01e51
Stenio Wagner <stenio.wagner1@gmail.com>
2020-08-29 23:19:48 -0300

    feat: finishing the header of the famous-detail screen

--- src/components/screens/shared/famous-detail/header/birthday-text/BirthDayText.spec.tsx
+++ src/components/screens/shared/famous-detail/header/birthday-text/BirthDayText.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup, render } from 'react-native-testing-library';
+import { cleanup, render, act } from 'react-native-testing-library';
 import { ThemeProvider } from 'styled-components';
 
 import { dark } from 'styles/themes';
@@ -26,10 +26,18 @@ const renderBirthDayText = (birthDate: string) => (
 );
 
 describe('Testing <BirthDatText />', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
   afterEach(cleanup);
 
   it('it should return render correctly with a valid date', () => {
     const { getByText } = render(renderBirthDayText('1994-02-21'));
+
+    act(() => {
+      jest.runAllTimers();
+    });
 
     expect(getByText('February 21, 1994')).not.toBeNull();
   });
@@ -37,11 +45,19 @@ describe('Testing <BirthDatText />', () => {
   it('it should return render correctly with a valid date', () => {
     const { getByText } = render(renderBirthDayText('1994-02-28'));
 
+    act(() => {
+      jest.runAllTimers();
+    });
+
     expect(getByText('February 28, 1994')).not.toBeNull();
   });
 
   it('it should return render correctly with a valid date', () => {
     const { getByText } = render(renderBirthDayText('2020-08-29'));
+
+    act(() => {
+      jest.runAllTimers();
+    });
 
     expect(getByText('August 29, 2020')).not.toBeNull();
   });
