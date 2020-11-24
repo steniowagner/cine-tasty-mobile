ae529f6704b6dbc81b6fe35ffd4465fa23345f95
Stenio Wagner <stenio.wagner1@gmail.com>
2020-11-24 20:36:11 -0300

    wip

---
+++ src/components/common/dynamic-image/DynamicImage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const DynamicImage = () => (
+  <View
+    style={{
+      width: '100%',
+      height: '100%',
+      backgroundColor: '#f0f',
+    }}
+  />
+);
+
+export default DynamicImage;
