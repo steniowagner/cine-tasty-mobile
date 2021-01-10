32d7c22f09c46ef9095397cd0b23a4be17877e74
Stenio Wagner <stenio.wagner1@gmail.com>
2021-01-09 22:35:55 -0300

    installing react-navigation-splash-screen

---
+++ src/routes/components/RootNavigation.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SearchStack, {
+  StackID as SearchStackID,
+} from 'components/screens/shared/search/routes/stack-routes';
+
+import ImagesGalleryModal, {
+  MODAL_ID as IMAGES_GALLERY_MODAL_ID,
+} from 'components/screens/shared/images-gallery/routes/stack-routes';
+
+import CustomModal, {
+  MODAL_ID as CUSTOM_MODAL_ID,
+} from 'components/screens/shared/customized-modal/routes/stack-routes';
+
+import Tabs from './Tabs';
+
+const RootStack = createStackNavigator();
+
+const RouteNavigation = () => (
+  <RootStack.Navigator
+    screenOptions={{
+      cardStyleInterpolator: ({ current: { progress } }) => ({
+        cardStyle: {
+          opacity: progress.interpolate({
+            inputRange: [0, 0.5, 0.9, 1],
+            outputRange: [0, 0.25, 0.7, 1],
+          }),
+        },
+        overlayStyle: {
+          opacity: progress.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 0.5],
+            extrapolate: 'clamp',
+          }),
+        },
+      }),
+      cardStyle: { backgroundColor: 'transparent' },
+      cardOverlayEnabled: true,
+      animationEnabled: false,
+      headerShown: false,
+    }}
+    mode="modal"
+  >
+    <RootStack.Screen
+      options={{ headerShown: false }}
+      component={Tabs}
+      name="Tabs"
+    />
+    <RootStack.Screen
+      options={{ headerShown: false, animationEnabled: true }}
+      component={SearchStack}
+      name={SearchStackID}
+    />
+    <RootStack.Screen
+      options={{ headerShown: false, animationEnabled: true }}
+      component={ImagesGalleryModal}
+      name={IMAGES_GALLERY_MODAL_ID}
+    />
+    <RootStack.Screen
+      options={{ headerShown: false, animationEnabled: true }}
+      component={CustomModal}
+      name={CUSTOM_MODAL_ID}
+    />
+  </RootStack.Navigator>
+);
+
+export default RouteNavigation;
---
+++ src/routes/components/Tabs.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  createBottomTabNavigator,
+  BottomTabBarProps,
+} from '@react-navigation/bottom-tabs';
+
+import TabNavigator from 'components/utils/tab-navigator/TabNavigator';
+
+import HomeStack, {
+  TabID as HomeTabID,
+} from 'components/screens/home/routes/stack-routes';
+
+import FamousStack, {
+  TabID as FamousTabID,
+} from 'components/screens/famous/routes/stack-routes';
+
+import QuizStack, {
+  TabID as QuizTabID,
+} from 'components/screens/quiz/routes/stack-routes';
+
+import NewsStack, {
+  TabID as NewsTabID,
+} from 'components/screens/news/routes/stack-routes';
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => (
+  <Tab.Navigator
+    initialRouteName={HomeTabID}
+    tabBar={(props: BottomTabBarProps) => (
+      <TabNavigator
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...props}
+      />
+    )}
+  >
+    <Tab.Screen
+      component={HomeStack}
+      name={HomeTabID}
+    />
+    <Tab.Screen
+      component={FamousStack}
+      name={FamousTabID}
+    />
+    <Tab.Screen
+      component={QuizStack}
+      name={QuizTabID}
+    />
+    <Tab.Screen
+      component={NewsStack}
+      name={NewsTabID}
+    />
+  </Tab.Navigator>
+);
+
+export default Tabs;
