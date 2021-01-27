14cdbabf5d1f17da20e6974f2ef3f2745f9acf66
Stenio Wagner <stenio.wagner1@gmail.com>
2021-01-27 19:45:18 -0300

    removing padding-top from the custom-modal-screen in order to show the content properly

--- src/components/screens/shared/customized-modal/components/CustomizedModal.tsx
+++ src/components/screens/shared/customized-modal/components/CustomizedModal.tsx
@@ -21,7 +21,6 @@ const Wrapper = styled(View)`
   width: 100%;
   height: 100%;
   justify-content: flex-end;
-  padding-top: ${({ theme }) => theme.metrics.height}px;
   background-color: ${({ theme }) => theme.colors.darkLayer};
 `;
 
