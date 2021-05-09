5c11b66d77f23e6b1da92ea95be562fa99558759
Stenio Wagner <stenio.wagner1@gmail.com>
2021-05-08 21:48:42 -0300

    refactoring home-screen-loading

--- src/components/screens/home/components/top3/LoadingTop3.tsx
+++ src/components/screens/home/components/top3/LoadingTop3.tsx
@@ -1,52 +1,41 @@
 import React from 'react';
-import { FlatList } from 'react-native';
 
 import LoadingPlaceholder from '@components/common/loading-placeholder/LoadingPlaceholder';
 
 import * as Styles from './LoadingTop3.styles';
 
-const loadingItems = Array(3)
-  .fill(0)
-  .map((_item, index) => index);
+const DEFAULT_STYLE = {
+  borderRadius: Styles.ITEM_BORDER_RADIUS,
+  height: Styles.ITEM_HEIGHT,
+  width: Styles.ITEM_WIDTH,
+};
 
 const LoadingTop3 = () => (
-  <Styles.ListWrapper
+  <Styles.Wrapper
     testID="loading-home"
   >
-    <FlatList
-      ListHeaderComponent={() => <Styles.GapFlatlist />}
-      ListFooterComponent={() => <Styles.GapFlatlist />}
-      renderItem={({ index }) => (
-        <>
-          <LoadingPlaceholder
-            indexToDelayAnimation={index}
-            style={{
-              marginHorizontal: index === 1 ? Styles.ITEM_MARGING_HORIZONTAL : 0,
-              marginTop: index !== 1 ? Styles.ITEM_MARGING_TOP : 0,
-              borderRadius: Styles.ITEM_BORDER_RADIUS,
-              height: Styles.ITEM_HEIGHT,
-              width: Styles.ITEM_WIDTH,
-            }}
-          />
-          <Styles.LoadingSmokeShadow
-            isTheMiddle={index === 1}
-          />
-        </>
-      )}
-      showsHorizontalScrollIndicator={false}
-      getItemLayout={(_data, index) => ({
-        offset: Styles.ITEM_WIDTH * index,
-        length: Styles.ITEM_WIDTH,
-        index,
-      })}
-      keyExtractor={(item) => `${item}`}
-      initialScrollIndex={1}
-      scrollEnabled={false}
-      data={loadingItems}
-      bounces={false}
-      horizontal
-    />
-  </Styles.ListWrapper>
+    <Styles.RightItemWrapper>
+      <LoadingPlaceholder
+        indexToDelayAnimation={0}
+        style={{ ...DEFAULT_STYLE }}
+      />
+      <Styles.LoadingSmokeShadow />
+    </Styles.RightItemWrapper>
+    <Styles.CenterItemWrapper>
+      <LoadingPlaceholder
+        indexToDelayAnimation={1}
+        style={{ ...DEFAULT_STYLE }}
+      />
+      <Styles.LoadingSmokeShadow />
+    </Styles.CenterItemWrapper>
+    <Styles.LeftItemWrapper>
+      <LoadingPlaceholder
+        indexToDelayAnimation={2}
+        style={{ ...DEFAULT_STYLE }}
+      />
+      <Styles.LoadingSmokeShadow />
+    </Styles.LeftItemWrapper>
+  </Styles.Wrapper>
 );
 
 export default LoadingTop3;
