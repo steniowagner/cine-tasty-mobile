408be186dd0850607012ab008cd73b7bdf3d4777
CI <ci@getflex.com>
2021-10-09 16:44:49 -0300

    fixing android-navigation-bar-color depednding on the theme or if the user is on onboarding

--- src/routes/components/Tabs.tsx
+++ src/routes/components/Tabs.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import {
   createBottomTabNavigator,
   BottomTabBarProps,
 } from '@react-navigation/bottom-tabs';
 import { Routes } from '@routes/routes';
 
+import AndroidNavigationBar from '@components/utils/AndroidNavigationBar.android';
 import FamousStack from '@components/screens/famous/routes/stack-routes';
 import TabNavigator from '@components/utils/tab-navigator/TabNavigator';
 import HomeStack from '@components/screens/home/routes/stack-routes';
@@ -14,32 +16,35 @@ import NewsStack from '@components/screens/news/routes/stack-routes';
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-  <Tab.Navigator
-    initialRouteName={Routes.Home.HOME}
-    tabBar={(props: BottomTabBarProps) => (
-      <TabNavigator
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...props}
+  <>
+    <Tab.Navigator
+      initialRouteName={Routes.Home.HOME}
+      tabBar={(props: BottomTabBarProps) => (
+        <TabNavigator
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...props}
+        />
+      )}
+    >
+      <Tab.Screen
+        component={HomeStack}
+        name={Routes.Home.HOME}
       />
-    )}
-  >
-    <Tab.Screen
-      component={HomeStack}
-      name={Routes.Home.HOME}
-    />
-    <Tab.Screen
-      component={FamousStack}
-      name={Routes.Famous.FAMOUS}
-    />
-    <Tab.Screen
-      component={QuizStack}
-      name={Routes.Quiz.QUIZ}
-    />
-    <Tab.Screen
-      component={NewsStack}
-      name={Routes.News.NEWS}
-    />
-  </Tab.Navigator>
+      <Tab.Screen
+        component={FamousStack}
+        name={Routes.Famous.FAMOUS}
+      />
+      <Tab.Screen
+        component={QuizStack}
+        name={Routes.Quiz.QUIZ}
+      />
+      <Tab.Screen
+        component={NewsStack}
+        name={Routes.News.NEWS}
+      />
+    </Tab.Navigator>
+    {Platform.OS === 'android' && <AndroidNavigationBar />}
+  </>
 );
 
 export default Tabs;
