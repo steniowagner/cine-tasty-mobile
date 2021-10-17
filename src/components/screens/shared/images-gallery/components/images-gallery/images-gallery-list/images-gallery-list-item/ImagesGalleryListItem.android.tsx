0b680889e7ebafadddf242a32ad6025f5aecf994
CI <ci@getflex.com>
2021-10-17 18:32:49 -0300

    refactoring the ImagesGalleryList component

--- src/components/screens/shared/images-gallery/components/images-gallery/images-gallery-list/images-gallery-list-item/ImagesGalleryListItem.android.tsx
+++
@@ -1,8 +0,0 @@
-import { memo } from 'react';
-import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
-
-import ImagesGalleryListItemComponent from './ImagesGalleryListItemComponent';
-
-const ImagesGalleryListItem = gestureHandlerRootHOC(ImagesGalleryListItemComponent);
-
-export default memo(ImagesGalleryListItem, (): boolean => true);
