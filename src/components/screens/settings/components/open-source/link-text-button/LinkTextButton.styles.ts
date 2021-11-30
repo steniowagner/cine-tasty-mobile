6b095dd8fa72cc798b199ce9d115de7705fce9f7
CI <ci@getflex.com>
2021-11-30 18:48:08 -0300

    removing LinkButton component from the open-source screen

--- src/components/screens/settings/components/open-source/link-text-button/LinkTextButton.styles.ts
+++
@@ -1,6 +0,0 @@
-import { TouchableOpacity } from 'react-native';
-import styled from 'styled-components';
-
-export const LinkButtonWrapper = styled(TouchableOpacity)`
-  margin-top: ${({ theme }) => theme.metrics.mediumSize}px;
-`;
