cb14b9610d3fa74d1ce2dab635385a9029d10ede
CI <ci@getflex.com>
2022-01-16 17:39:01 -0300

    upgrade: setting up styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
+import {View, Text} from 'react-native';
 import {ApolloProvider} from '@apollo/client';
+import styled, {ThemeProvider} from 'styled-components/native';
+
+import {TMDBImageQualityProvider, ThemeContextProvider} from '@providers';
+import {dark, light} from '@styles/themes';
 
 import makeGraphQLClient from './graphql/client';
-import Test from './Test';
 
 const client = makeGraphQLClient();
 
 const App = () => (
-  <ApolloProvider client={client}>
-    <Test />
-  </ApolloProvider>
+  <ThemeProvider theme={light}>
+    <Wrapper>
+      <VotesText>123</VotesText>
+    </Wrapper>
+  </ThemeProvider>
 );
+
+const Wrapper = styled(View)`
+  width: 100%;
+  height: 100%;
+  background-color: #0f0;
+  justify-content: center;
+  align-items: center;
+`;
+
+const VotesText = styled(Text)`
+  font-size: ${({theme}) => theme.metrics.extraLargeSize * 10}px;
+  color: ${({theme}) => theme.colors.text};
+  font-family: CircularStd-Black;
+`;
 
 export default App;
 
@@ -23,7 +43,6 @@ import makeClient from './graphql/client';
 const client = makeClient();
 
 const App = () => (
-  <ThemeContextProvider>
     <TMDBImageQualityProvider>
       <>
         <ApolloProvider
@@ -35,7 +54,6 @@ const App = () => (
         </ApolloProvider>
       </>
     </TMDBImageQualityProvider>
-  </ThemeContextProvider>
 );
 
 export default App;
