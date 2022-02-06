51061339aee917d08a9f843db97b880821b08c16
CI <ci@getflex.com>
2022-02-06 18:36:48 -0300

    refact: NewsLoading

---
+++ src/components/screens/news/components/loading-list/NewsLoading.styles.ts
@@ -0,0 +1,6 @@
+import {View} from 'react-native';
+import styled from 'styled-components/native';
+
+export const LoadingList = styled(View)`
+  flex: 1;
+`;
--- src/components/screens/news/components/loading-list/NewsLoading.tsx
+++ src/components/screens/news/components/loading-list/NewsLoading.tsx
@@ -1,29 +1,24 @@
 import React from 'react';
-import {ScrollView} from 'react-native';
-import styled from 'styled-components/native';
 
 import metrics from '@styles/metrics';
 
 import NewsListLoadingPlaceholderItem from './NewsListLoadingPlaceholderItem';
-import * as NewsListItemStyles from '../list-item/NewsListItem.styles';
+import {imageWrapper} from '../list-item/NewsListItem.styles';
+import * as Styles from './NewsLoading.styles';
 
 export const INITIAL_ITEMS_TO_RENDER =
-  Math.floor(metrics.height / NewsListItemStyles.imageWrapper.height) - 1;
+  Math.floor(metrics.height / imageWrapper.height) - 1;
 
 const newsLoadingItems = Array(INITIAL_ITEMS_TO_RENDER)
   .fill(0)
   .map((_, index) => `${index}`);
 
 const NewsLoading = () => (
-  <LoadingList testID="news-loading-list" scrollEnabled={false} bounces={false}>
+  <Styles.LoadingList testID="news-loading-list">
     {newsLoadingItems.map((newsLoadingItem, index) => (
       <NewsListLoadingPlaceholderItem index={index} key={newsLoadingItem} />
     ))}
-  </LoadingList>
+  </Styles.LoadingList>
 );
-
-const LoadingList = styled(ScrollView)`
-  flex: 1;
-`;
 
 export default NewsLoading;
