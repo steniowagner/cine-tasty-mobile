4be8d8e8364737b0c42f82c02471218cf654c103
CI <ci@getflex.com>
2022-02-15 20:04:24 -0300

    refact: refactoring NoQuestionsError

---
+++ src/components/screens/quiz/components/questions/no-questions-error/NoQuestionsError.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {cleanup, render, RenderAPI} from '@testing-library/react-native';
+import {ThemeProvider} from 'styled-components/native';
+
+import {dark as theme} from '@styles/themes/dark';
+import {Translations} from '@i18n/tags';
+
+import NoQuestionsError from './NoQuestionsError';
+
+const renderNoQuestionsError = () => (
+  <ThemeProvider theme={theme}>
+    <NoQuestionsError />
+  </ThemeProvider>
+);
+
+describe('<NoQuestionsError />', () => {
+  const elements = {
+    descriptionText: (api: RenderAPI) =>
+      api.queryByTestId('advise-description'),
+    suggestionText: (api: RenderAPI) => api.queryByTestId('advise-suggestion'),
+    titleText: (api: RenderAPI) => api.queryByTestId('advise-title'),
+    wrapper: (api: RenderAPI) =>
+      api.queryByTestId('no-questions-error-wrapper'),
+    playlistIcon: (api: RenderAPI) => api.queryByTestId('icon-playlist-remove'),
+  };
+
+  afterEach(cleanup);
+
+  it('should renders correctly', () => {
+    const component = render(renderNoQuestionsError());
+    expect(elements.wrapper(component)).not.toBeNull();
+    expect(elements.playlistIcon(component)).not.toBeNull();
+    expect(elements.descriptionText(component)).not.toBeNull();
+    expect(elements.descriptionText(component).children[0]).toEqual(
+      Translations.Tags.QUIZ_NO_QUESTIONS_ADVISE_DESCRIPTION,
+    );
+    expect(elements.suggestionText(component)).not.toBeNull();
+    expect(elements.suggestionText(component).children[0]).toEqual(
+      Translations.Tags.QUIZ_NO_QUESTIONS_ADVISE_SUGGESTION,
+    );
+    expect(elements.titleText(component)).not.toBeNull();
+    expect(elements.titleText(component).children[0]).toEqual(
+      Translations.Tags.QUIZ_NO_QUESTIONS_ADVISE_TITLE,
+    );
+  });
+});
---
+++ src/components/screens/quiz/components/questions/no-questions-error/NoQuestionsError.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Advise from '@components/common/advise/Advise';
+
+import useNoQuestionsError from './useNoQuestionsError';
+
+import * as Styles from '../Questions.styles';
+
+const NoQuestionError = () => {
+  const noQuestionsError = useNoQuestionsError();
+  return (
+    <Styles.ErrorWrapper testID="no-questions-error-wrapper">
+      <Advise
+        description={noQuestionsError.texts.description}
+        suggestion={noQuestionsError.texts.suggestion}
+        title={noQuestionsError.texts.title}
+        icon="playlist-remove"
+      />
+    </Styles.ErrorWrapper>
+  );
+};
+
+export default NoQuestionError;
