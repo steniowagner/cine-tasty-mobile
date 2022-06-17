78a8557ccb8f6e059c5d2e084861e268bfb1302e
Stenio Wagner <stenio.wagner1@gmail.com>
2022-06-17 13:23:05 -0300

    feat: adjusting famous-cast-lists padding-left

--- src/components/common/scrollable-section/ScrollableSection.styles.ts
+++ src/components/common/scrollable-section/ScrollableSection.styles.ts
@@ -3,9 +3,11 @@ import styled from 'styled-components/native';
 
 import {CONSTANTS} from '@utils';
 
-export const ScrollViewSection = styled(ScrollView)`
-  padding-left: ${CONSTANTS.VALUES.DEFAULT_SPACING}px;
-`;
+export const ScrollViewSection = styled(ScrollView).attrs(() => ({
+  contentContainerStyle: {
+    paddingLeft: CONSTANTS.VALUES.DEFAULT_SPACING,
+  },
+}))``;
 
 export const FlatListSection = styled(FlatList).attrs(() => ({
   contentContainerStyle: {
