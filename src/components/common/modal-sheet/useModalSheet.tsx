117fd621da01a3ef1b5415a675091aecee368044
Stenio Wagner <stenio.wagner1@gmail.com>
2023-01-12 16:38:37 -0300

    feat: removing the isOpen logic from the useModalSheet

--- src/components/common/modal-sheet/useModalSheet.tsx
+++ src/components/common/modal-sheet/useModalSheet.tsx
@@ -7,8 +7,6 @@ import {
   MAX_CLAMPING,
 } from './useAnimateModalDistanceFromTop';
 
-const TIMEOUT_TO_CLOSE_MODAL = 1000;
-
 type UseModalSheetProps = {
   height?: number;
   onClose: () => void;
@@ -16,38 +14,21 @@ type UseModalSheetProps = {
 };
 
 export const useModalSheet = (props: UseModalSheetProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleCloseModal = useCallback(() => {
-    setIsOpen(false);
-    setTimeout(() => {
-      props.onClose();
-    }, TIMEOUT_TO_CLOSE_MODAL);
-  }, [props.onClose]);
-
   const cardHeight = useMemo(
     () => props.height ?? DEFAULT_MODAL_SHEET_HEIGHT,
     [props.height],
   );
 
-  const animateDarkLayerOpacity = useAnimateDarkLayerOpacity({
-    isOpen,
-  });
-
   const animateModalDistanceFromTop = useAnimateModalDistanceFromTop({
-    closeModal: handleCloseModal,
+    closeModal: props.onClose,
     cardHeight,
-    isOpen,
+    isOpen: props.isOpen,
   });
-
-  useEffect(() => {
-    setIsOpen(props.isOpen);
-  }, [props.isOpen]);
 
   return {
     cardAnimatedStyle: animateModalDistanceFromTop.animatedStyle,
     handleGestureEvent: animateModalDistanceFromTop.handleGestureEvent,
-    darkLayerAnimatedStyle: animateDarkLayerOpacity.animatedStyle,
-    height: cardHeight + Math.abs(MAX_CLAMPING),
+    darkLayerAnimatedStyle: animateModalDistanceFromTop.darkLayerAnimatedStyle,
+    height: cardHeight + MAX_CLAMPING,
   };
 };
