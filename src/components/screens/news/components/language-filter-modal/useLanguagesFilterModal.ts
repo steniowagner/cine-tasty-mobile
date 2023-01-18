08ed51c8faae92b2823d2b9437252986c3d6208b
Stenio Wagner <stenio.wagner1@gmail.com>
2023-01-18 00:53:42 -0300

    feat: creating the LanguagesFilterModal as wrapper for the ModaSheet

---
+++ src/components/screens/news/components/language-filter-modal/useLanguagesFilterModal.ts
@@ -0,0 +1,40 @@
+import {useCallback, useMemo, useState} from 'react';
+
+import * as SchemaTypes from '@schema-types';
+import {useTranslations} from '@hooks';
+import {Translations} from '@i18n/tags';
+
+type UseLanguagesFilterModalProps = {
+  lastLanguageSelected: SchemaTypes.ArticleLanguage;
+  onSelectLanguage: (language: string) => void;
+  onCloseModal: () => void;
+};
+
+export const useLanguagesFilterModal = (
+  props: UseLanguagesFilterModalProps,
+) => {
+  const [languageSelected, setLanguageSelected] =
+    useState<SchemaTypes.ArticleLanguage>(props.lastLanguageSelected);
+
+  const translations = useTranslations();
+
+  const texts = useMemo(
+    () => ({
+      modalCtaButton: translations.translate(Translations.Tags.SELECT),
+      modalTitle: translations.translate(Translations.Tags.NEWS_FILTER_MESSAGE),
+    }),
+    [translations.translate],
+  );
+
+  const handlePressSelect = useCallback(() => {
+    props.onSelectLanguage(languageSelected);
+    props.onCloseModal();
+  }, [props.onCloseModal, props.onSelectLanguage, languageSelected]);
+
+  return {
+    languageSelected,
+    onPressSelect: handlePressSelect,
+    setLanguageSelected,
+    texts,
+  };
+};
