890758145effff603a86065664621dfe31fcb6a1
Stenio Wagner <stenio.wagner1@gmail.com>
2023-01-18 13:00:48 -0300

    feat: exporting the tab-navigator height

--- src/routes/components/tab-navigator/TabNavigator.styles.ts
+++ src/routes/components/tab-navigator/TabNavigator.styles.ts
@@ -2,12 +2,14 @@ import {View} from 'react-native';
 import styled from 'styled-components/native';
 
 import {isEqualsOrLargestThanIphoneX} from '@utils';
+import metrics from '@styles/metrics';
+
+export const WRAPPER_HEIGHT =
+  metrics.getWidthFromDP('18%') + (isEqualsOrLargestThanIphoneX() ? 30 : 0);
 
 export const Wrapper = styled(View)`
   width: ${({theme}) => theme.metrics.width}px;
-  height: ${({theme}) =>
-    theme.metrics.getWidthFromDP('18%') +
-    (isEqualsOrLargestThanIphoneX() ? 30 : 0)}px;
+  height: ${WRAPPER_HEIGHT}px;
   flex-direction: row;
   background-color: ${({theme}) => theme.colors.secondary};
   padding-bottom: ${isEqualsOrLargestThanIphoneX() ? 30 : 0}px;
