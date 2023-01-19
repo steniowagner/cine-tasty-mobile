8d5310bc12b755c0726cb89ead95929275f7e7bf
Stenio Wagner <stenio.wagner1@gmail.com>
2023-01-19 11:52:17 -0300

    refact: small refacts at the DropdownOption component

--- src/components/screens/quiz/components/setup-questions/drop-down-option/DropdownOption.styles.tsx
+++ src/components/screens/quiz/components/setup-questions/drop-down-option/DropdownOption.styles.tsx
@@ -1,6 +1,10 @@
 import {TouchableOpacity, Text} from 'react-native';
 import styled from 'styled-components/native';
 
+import metrics from '@styles/metrics';
+
+export const ICON_SIZE = metrics.getWidthFromDP('7%');
+
 export const InnerContentWrapper = styled(TouchableOpacity)`
   width: 100%;
   flex-direction: row;
--- src/components/screens/quiz/components/setup-questions/drop-down-option/DropdownOption.tsx
+++ src/components/screens/quiz/components/setup-questions/drop-down-option/DropdownOption.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import {SVGIcon} from '@components';
-import metrics from '@styles/metrics';
 import * as Types from '@local-types';
 
 import * as Styles from './DropdownOption.styles';
@@ -12,15 +11,13 @@ type DropdownOptionProps = {
   onPress: () => void;
 };
 
-const DropdownOption = (props: DropdownOptionProps) => (
+export const DropdownOption = (props: DropdownOptionProps) => (
   <Styles.InnerContentWrapper
     onPress={props.onPress}
     testID={`dropdown-button-${props.option}`}>
     <Styles.OptionText testID={`dropdown-value-${props.option}`}>
       {props.selectedOption}
     </Styles.OptionText>
-    <SVGIcon size={metrics.getWidthFromDP('7%')} id="chevron-down-box" />
+    <SVGIcon size={Styles.ICON_SIZE} id="chevron-down-box" />
   </Styles.InnerContentWrapper>
 );
-
-export default DropdownOption;
