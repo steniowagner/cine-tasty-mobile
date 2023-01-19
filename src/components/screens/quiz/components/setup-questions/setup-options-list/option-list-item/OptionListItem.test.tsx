d85853ec76b1ab946c1efbc3fd74ced525663121
Stenio Wagner <stenio.wagner1@gmail.com>
2023-01-19 15:54:16 -0300

    test: small refactories on the OptionListItem component

--- src/components/screens/quiz/components/setup-questions/setup-options-list/option-list-item/OptionListItem.test.tsx
+++ src/components/screens/quiz/components/setup-questions/setup-options-list/option-list-item/OptionListItem.test.tsx
@@ -12,13 +12,37 @@ import {dark as theme} from '@styles/themes/dark';
 
 import OptionListItem from './OptionListItem';
 
-const renderOptionListItem = (
-  title: string,
-  isSelected = true,
-  onPress = jest.fn(),
-) => (
+const TITLE = 'ANY_TITLE';
+
+const checkIsProperlySelected = (
+  component: RenderAPI,
+  elements: Record<string, any>,
+) => {
+  expect(elements.optionListItemButton(component)).not.toBeNull();
+  expect(elements.optionListItemText(component)).not.toBeNull();
+  expect(elements.optionListItemText(component).children[0]).toEqual(TITLE);
+  expect(elements.checkboxCircle(component)).not.toBeNull();
+  expect(
+    elements.optionListItemButton(component).props.style.backgroundColor,
+  ).toEqual(theme.colors.primary);
+};
+
+const checkIsProperlyUnselected = (
+  component: RenderAPI,
+  elements: Record<string, any>,
+) => {
+  expect(elements.optionListItemButton(component)).not.toBeNull();
+  expect(elements.optionListItemText(component)).not.toBeNull();
+  expect(elements.optionListItemText(component).children[0]).toEqual(TITLE);
+  expect(elements.checkboxCircle(component)).toBeNull();
+  expect(
+    elements.optionListItemButton(component).props.style.backgroundColor,
+  ).toEqual('transparent');
+};
+
+const renderOptionListItem = (isSelected = true, onPress = jest.fn()) => (
   <ThemeProvider theme={theme}>
-    <OptionListItem onPress={onPress} isSelected={isSelected} title={title} />
+    <OptionListItem onPress={onPress} isSelected={isSelected} title={TITLE} />
   </ThemeProvider>
 );
 
@@ -31,7 +55,6 @@ describe('<OptionListItem />', () => {
     checkboxCircle: (api: RenderAPI) =>
       api.queryByTestId('icon-checkbox-circle'),
   };
-  const title = 'ANY_TITLE';
 
   beforeEach(() => {
     jest.useFakeTimers();
@@ -40,69 +63,51 @@ describe('<OptionListItem />', () => {
   afterEach(cleanup);
 
   it('should render correctly when "isSelected" is "true"', () => {
-    const component = render(renderOptionListItem(title));
+    const component = render(renderOptionListItem());
     act(() => {
       jest.runAllTimers();
     });
-    expect(elements.optionListItemButton(component)).not.toBeNull();
-    expect(elements.optionListItemText(component)).not.toBeNull();
-    expect(elements.optionListItemText(component).children[0]).toEqual(title);
-    expect(elements.checkboxCircle(component)).not.toBeNull();
+    checkIsProperlySelected(component, elements);
   });
 
   it('should render correctly when "isSelected" is "false"', () => {
-    const component = render(renderOptionListItem(title, false));
+    const component = render(renderOptionListItem(false));
     act(() => {
       jest.runAllTimers();
     });
-    expect(elements.optionListItemButton(component)).not.toBeNull();
-    expect(elements.optionListItemText(component)).not.toBeNull();
-    expect(elements.optionListItemText(component).children[0]).toEqual(title);
-    expect(elements.checkboxCircle(component)).toBeNull();
+    checkIsProperlyUnselected(component, elements);
   });
 
   it('should render correctly when "isSelected" is previously "false" and then it changes to "true"', () => {
-    const component = render(renderOptionListItem(title, false));
+    const component = render(renderOptionListItem(false));
     act(() => {
       jest.runAllTimers();
     });
-    expect(elements.optionListItemButton(component)).not.toBeNull();
-    expect(elements.optionListItemText(component)).not.toBeNull();
-    expect(elements.optionListItemText(component).children[0]).toEqual(title);
-    expect(elements.checkboxCircle(component)).toBeNull();
-    component.rerender(renderOptionListItem(title, true));
-    expect(elements.optionListItemButton(component)).not.toBeNull();
-    expect(elements.optionListItemText(component)).not.toBeNull();
-    expect(elements.optionListItemText(component).children[0]).toEqual(title);
-    expect(elements.checkboxCircle(component)).not.toBeNull();
+    checkIsProperlyUnselected(component, elements);
+    component.rerender(renderOptionListItem(true));
+    checkIsProperlySelected(component, elements);
   });
 
   it('should render correctly when "isSelected" is previously "true" and then it changes to "false"', () => {
-    const component = render(renderOptionListItem(title, false));
+    const component = render(renderOptionListItem(true));
     act(() => {
       jest.runAllTimers();
     });
-    expect(elements.optionListItemButton(component)).not.toBeNull();
-    expect(elements.optionListItemText(component)).not.toBeNull();
-    expect(elements.optionListItemText(component).children[0]).toEqual(title);
-    expect(elements.checkboxCircle(component)).toBeNull();
-    component.rerender(renderOptionListItem(title, true));
-    expect(elements.optionListItemButton(component)).not.toBeNull();
-    expect(elements.optionListItemText(component)).not.toBeNull();
-    expect(elements.optionListItemText(component).children[0]).toEqual(title);
-    expect(elements.checkboxCircle(component)).not.toBeNull();
+    checkIsProperlySelected(component, elements);
+    component.rerender(renderOptionListItem(false));
+    checkIsProperlyUnselected(component, elements);
   });
 
   it('should call "onPress" when the user presses the item and it is selected', () => {
     const onPress = jest.fn();
-    const component = render(renderOptionListItem(title, true, onPress));
+    const component = render(renderOptionListItem(true, onPress));
     fireEvent.press(elements.optionListItemButton(component));
     expect(onPress).toHaveBeenCalledTimes(1);
   });
 
   it('should call "onPress" when the user presses the item and it is not selected', () => {
     const onPress = jest.fn();
-    const component = render(renderOptionListItem(title, false, onPress));
+    const component = render(renderOptionListItem(false, onPress));
     fireEvent.press(elements.optionListItemButton(component));
     expect(onPress).toHaveBeenCalledTimes(1);
   });
