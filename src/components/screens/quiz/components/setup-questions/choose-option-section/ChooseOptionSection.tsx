5c3cd14b3328f85c31b65cd3ac948281bbda2dd5
Stenio Wagner <stenio.wagner1@gmail.com>
2023-01-22 16:00:30 -0300

    refact: refactoring the ChooseOptionSection

--- src/components/screens/quiz/components/setup-questions/choose-option-section/ChooseOptionSection.tsx
+++ src/components/screens/quiz/components/setup-questions/choose-option-section/ChooseOptionSection.tsx
@@ -3,22 +3,30 @@ import React from 'react';
 import * as Types from '@local-types';
 
 import {DropdownOption} from './drop-down-option/DropdownOption';
-import * as Styles from './ChooseOptionSection.styles';
+import * as Styles from '../SetupQuestions.styles';
+import {useChooseOptionSection} from './useChooseOptionSection';
 
 type ChooseOptionSectionProps = {
-  label: string;
-  selectedOption: string;
-  onPress: () => void;
+  onOpenSetupQuestionsModal: () => void;
   section: Types.QuizOption;
 };
 
-export const ChooseOptionSection = (props: ChooseOptionSectionProps) => (
-  <>
-    <Styles.Label>{props.label}</Styles.Label>
-    <DropdownOption
-      onPress={props.onPress}
-      selectedOption={props.selectedOption}
-      section={props.section}
-    />
-  </>
-);
+export const ChooseOptionSection = (props: ChooseOptionSectionProps) => {
+  const chooseOptionSection = useChooseOptionSection({
+    onOpenSetupQuestionsModal: props.onOpenSetupQuestionsModal,
+    section: props.section,
+  });
+
+  return (
+    <>
+      <Styles.SectionTitle>
+        {chooseOptionSection.texts.sectionTitle}
+      </Styles.SectionTitle>
+      <DropdownOption
+        onPress={chooseOptionSection.onPressOption}
+        selectedOption={chooseOptionSection.selectedOption}
+        section={props.section}
+      />
+    </>
+  );
+};
