9ac0eadef8fcf57263c5a7c4f69f0fc65beabb34
Stenio Wagner <stenio.wagner1@gmail.com>
2023-01-25 22:58:17 -0300

    test: improving the tests for the QuestionWrapper

--- src/components/screens/quiz/components/questions/question-wrapper/QuestionWrapper.test.tsx
+++ src/components/screens/quiz/components/questions/question-wrapper/QuestionWrapper.test.tsx
@@ -4,21 +4,21 @@ import {cleanup, render, RenderAPI} from '@testing-library/react-native';
 import {ThemeProvider} from 'styled-components/native';
 
 import {dark as theme} from '@styles/themes/dark';
+import {randomPositiveNumber} from '@mocks/utils';
 
-import QuestionWrapper from './QuestionWrapper';
+import {QuestionWrapper} from './QuestionWrapper';
 
-const defaultProps = {
-  currentQuestionIndex: 1,
-  numberOfQuestions: 2,
-  question: 'ANY_QUESTION',
-};
+const DEFAULT_QUESTION = 'ANY_QUESTION';
 
-const renderQuestions = () => (
+const renderQuestions = (
+  currentQuestionIndex: number,
+  numberOfQuestions: number,
+) => (
   <ThemeProvider theme={theme}>
     <QuestionWrapper
-      currentQuestionIndex={defaultProps.currentQuestionIndex}
-      numberOfQuestions={defaultProps.numberOfQuestions}
-      question={defaultProps.question}>
+      currentQuestionIndex={currentQuestionIndex}
+      numberOfQuestions={numberOfQuestions}
+      question={DEFAULT_QUESTION}>
       <View testID="children" />
     </QuestionWrapper>
   </ThemeProvider>
@@ -26,24 +26,25 @@ const renderQuestions = () => (
 
 describe('<QuestionWrapper />', () => {
   const elements = {
-    cardWrapper: (api: RenderAPI) => api.queryByTestId('card-wrapper'),
     questionIndicatorText: (api: RenderAPI) =>
       api.queryByTestId('question-indicator-text'),
     questionText: (api: RenderAPI) => api.queryByTestId('question-text'),
-    children: (api: RenderAPI) => api.queryByTestId('question-text'),
+    children: (api: RenderAPI) => api.queryByTestId('children'),
   };
 
   afterEach(cleanup);
 
   it('should render correctly', () => {
-    const component = render(renderQuestions());
+    const currentQuestionIndex = randomPositiveNumber(10);
+    const numberOfQuestions = randomPositiveNumber(10);
+    const component = render(
+      renderQuestions(currentQuestionIndex, numberOfQuestions),
+    );
     expect(
       elements.questionIndicatorText(component).props.children.join(''),
-    ).toEqual(
-      `${defaultProps.currentQuestionIndex}/${defaultProps.numberOfQuestions}`,
-    );
+    ).toEqual(`${currentQuestionIndex}/${numberOfQuestions}`);
     expect(elements.questionText(component).props.children).toEqual(
-      defaultProps.question,
+      DEFAULT_QUESTION,
     );
     expect(elements.children).not.toBeNull();
   });
