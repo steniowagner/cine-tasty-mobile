da17ea148bccdc6a31467b094aa1167540d0b2e1
Stenio Wagner <stenio.wagner1@gmail.com>
2023-03-07 15:42:48 -0300

    refact: refactoring the BirthDayText component

--- src/components/screens/common/famous-details/components/header/birthday-text/BirthDayText.tsx
+++ src/components/screens/common/famous-details/components/header/birthday-text/BirthDayText.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 
 import InfoText, {DEFAULT_MARGIN_VERTICAL} from '../InfoText';
-import useBirthDayText from './useBirthDayText';
+import {useBirthDayText} from './useBirthDayText';
 
 type BirthdayTextProps = {
   rawBirthDate: string;
 };
 
-const BirthDayText = (props: BirthdayTextProps) => {
+export const BirthDayText = (props: BirthdayTextProps) => {
   const birthDayText = useBirthDayText({rawDateString: props.rawBirthDate});
+
   return (
     <InfoText marginBottom={DEFAULT_MARGIN_VERTICAL - 3} testID="birthday-text">
       {birthDayText.text}
     </InfoText>
   );
 };
-
-export default BirthDayText;
