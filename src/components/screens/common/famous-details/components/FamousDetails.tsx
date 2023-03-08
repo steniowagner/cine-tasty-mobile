3a0266b844e3195382e0dbc1a90e18f226cb767b
Stenio Wagner <stenio.wagner1@gmail.com>
2023-03-08 11:50:52 -0300

    refact: refactoring the FamousDetails screen

--- src/components/screens/common/famous-details/components/FamousDetails.tsx
+++ src/components/screens/common/famous-details/components/FamousDetails.tsx
@@ -12,10 +12,10 @@ import {
 } from '@components';
 
 import {FamousDetailStackProps} from '../routes/route-params-types';
-import HeaderInfo from './header/header-info/HeaderInfo';
+import {HeaderInfo} from './header/header-info/HeaderInfo';
 import useFamousDetail from './useFamousDetails';
 import * as Styles from './FamousDetails.styles';
-import DeathDay from './death-day/DeathDay';
+import {DeathDay} from './death-day/DeathDay';
 
 export const FamousDetails = withTheme((props: FamousDetailStackProps) => {
   const famousDetail = useFamousDetail({
@@ -53,8 +53,8 @@ export const FamousDetails = withTheme((props: FamousDetailStackProps) => {
           isLoading={famousDetail.isLoading}
           name={props.route.params.name}
         />
-        {!!famousDetail.famous?.deathday && (
-          <DeathDay deathDate={famousDetail.famous.deathday} />
+        {famousDetail.famous?.deathday && (
+          <DeathDay day={famousDetail.famous.deathday} />
         )}
         <Styles.BiographySectionWrapper testID="biography-section">
           <ExpansibleTextSection
@@ -63,22 +63,22 @@ export const FamousDetails = withTheme((props: FamousDetailStackProps) => {
             isLoading={famousDetail.isLoading}
           />
         </Styles.BiographySectionWrapper>
-        {!!famousDetail.famous && (
+        {famousDetail.famous && (
           <>
-            {!!famousDetail.famous.images && (
+            {famousDetail.famous.images && (
               <ImagesList
                 images={famousDetail.famous.images}
                 orientation="PORTRAIT"
               />
             )}
-            {!!famousDetail.famous.moviesCast && (
+            {famousDetail.famous.moviesCast && (
               <MediaHorizontalList
                 title={famousDetail.texts.castMoviesSection}
                 dataset={famousDetail.famous.moviesCast}
                 type="MOVIE"
               />
             )}
-            {!!famousDetail.famous.tvCast && (
+            {famousDetail.famous.tvCast && (
               <MediaHorizontalList
                 title={famousDetail.texts.castTvShowSection}
                 dataset={famousDetail.famous.tvCast}
