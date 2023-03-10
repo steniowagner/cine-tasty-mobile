2848ec931ed5eec9b91b1a66fcdf1bc8b2f17db5
Stenio Wagner <stenio.wagner1@gmail.com>
2023-03-10 10:54:04 -0300

    refact: small refactoring at ImagesGallery

--- src/components/screens/common/images-gallery/components/images-gallery/ImagesGallery.tsx
+++ src/components/screens/common/images-gallery/components/images-gallery/ImagesGallery.tsx
@@ -1,27 +1,23 @@
-import React, {useLayoutEffect} from 'react';
+import React, {useEffect} from 'react';
 
 import {HeaderIconButton} from '@components';
 
 import {ImagesGalleryList} from './images-gallery-list/ImagesGalleryList';
 import {ThumbsGalleryList} from './thumbs-gallery-list/ThumbsGalleryList';
 import {ImagesGalleryStackProps} from '../../routes/route-params-types';
-import useImageGallery from './useImagesGallery';
+import {useImagesGallery} from './useImagesGallery';
 import * as Styles from './ImagesGallery.styles';
 
 export const ImagesGallery = (props: ImagesGalleryStackProps) => {
-  const imagesGallery = useImageGallery({
+  const imagesGallery = useImagesGallery({
     indexFirstItemSelected: props.route.params.indexSelected,
   });
-  useLayoutEffect(() => {
+
+  useEffect(() => {
     props.navigation.setOptions({
       title: `${imagesGallery.indexImageSelected + 1}/${
         props.route.params.images.length
       }`,
-    });
-  }, [imagesGallery.indexImageSelected]);
-
-  useLayoutEffect(() => {
-    props.navigation.setOptions({
       headerLeft: () => (
         <HeaderIconButton
           onPress={() => props.navigation.goBack()}
@@ -30,7 +26,7 @@ export const ImagesGallery = (props: ImagesGalleryStackProps) => {
         />
       ),
     });
-  }, []);
+  }, [imagesGallery.indexImageSelected]);
 
   return (
     <Styles.Wrapper>
