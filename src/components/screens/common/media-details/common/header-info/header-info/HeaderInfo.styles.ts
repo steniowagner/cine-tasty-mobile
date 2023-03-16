a3e85463430582e38a52cd7ba46dddcfcbd7723e
Stenio Wagner <stenio.wagner1@gmail.com>
2023-03-16 16:49:41 -0300

    feat: replacing PosterImage component by the TMDBImageWithFallback at the MediaDetails/HeaderInfo

--- src/components/screens/common/media-details/common/header-info/header-info/HeaderInfo.styles.ts
+++ src/components/screens/common/media-details/common/header-info/header-info/HeaderInfo.styles.ts
@@ -1,5 +1,12 @@
 import styled from 'styled-components/native';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+
+import metrics from '@styles/metrics';
+
+export const POSTER_IMAGE_ICON_SIZE = metrics.getWidthFromDP('14%');
+export const POSTER_IMAGE_DEFAULT_WIDTH = metrics.getWidthFromDP('70%');
+export const POSTER_IMAGE_DEFAULT_HEIGHT = metrics.getHeightFromDP('50%');
+export const POSTER_IMAGE_DEFAULT_BORDER_RADIUS = metrics.mediumSize;
 
 type WrapperStylesProp = {
   headerHeight: number;
@@ -17,3 +24,11 @@ export const MediaHeadlineWrapper = styled(View)`
   margin-top: ${({theme}) => theme.metrics.largeSize}px;
   margin-bottom: ${({theme}) => theme.metrics.extraSmallSize}px;
 `;
+
+export const sheet = StyleSheet.create({
+  poster: {
+    width: POSTER_IMAGE_DEFAULT_WIDTH,
+    height: POSTER_IMAGE_DEFAULT_HEIGHT,
+    borderRadius: POSTER_IMAGE_DEFAULT_BORDER_RADIUS,
+  },
+});
--- src/components/screens/common/media-details/common/header-info/header-info/HeaderInfo.tsx
+++ src/components/screens/common/media-details/common/header-info/header-info/HeaderInfo.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import {useHeaderHeight} from '@react-navigation/elements';
 
-import {MediaHeadline} from '@components';
+import {MediaHeadline, TMDBImageWithFallback} from '@components';
 
-import {PosterImage} from '../poster-image/PosterImage';
 import * as Styles from './HeaderInfo.styles';
 import {Tags} from '../../sections/tags/Tags';
 
@@ -21,7 +20,15 @@ export const HeaderInfo = (props: HeaderInfoProps) => {
 
   return (
     <Styles.Wrapper headerHeight={headerHeight} testID="header-info-wrapper">
-      <PosterImage loadingIcon="video-vintage" image={props.poster} />
+      <TMDBImageWithFallback
+        iconImageLoading="video-vintage"
+        iconImageError="image-off"
+        imageType="poster"
+        style={Styles.sheet.poster}
+        iconSize={Styles.POSTER_IMAGE_ICON_SIZE}
+        image={props.poster}
+        testID="poster-image"
+      />
       <Styles.MediaHeadlineWrapper>
         <MediaHeadline
           title={props.title}
