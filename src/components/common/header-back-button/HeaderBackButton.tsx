e07e0ffd62c55d9f85b6561016ee906f59f8345b
Stenio Wagner <stenio.wagner1@gmail.com>
2023-03-18 13:38:16 -0300

    feat: removing background-circle-wrapper at the HeaderBackButton

--- src/components/common/header-back-button/HeaderBackButton.tsx
+++ src/components/common/header-back-button/HeaderBackButton.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import {Platform} from 'react-native';
+import {Platform, TouchableOpacity} from 'react-native';
+import {Colors} from 'styled-components/native';
 
 import {SVGIcon} from '@components';
 import metrics from '@styles/metrics';
 
-import * as Styles from './HeaderBackButton.styles';
-
 type HeaderBackButtonProps = {
   onPress: () => void;
+  color: keyof Colors;
 };
 
 export const HeaderBackButton = (props: HeaderBackButtonProps) => (
-  <Styles.IconWrapper testID="header-back-button" onPress={props.onPress}>
+  <TouchableOpacity testID="header-back-button" onPress={props.onPress}>
     <SVGIcon
       size={Platform.select({
         android: metrics.getWidthFromDP('6.5%'),
-        ios: metrics.getWidthFromDP('8.7%'),
+        ios: metrics.getWidthFromDP('9%'),
       })}
       id={Platform.select({
         android: 'arrow-back',
         ios: 'chevron-left',
       })}
-      colorThemeRef="white"
+      colorThemeRef={props.color}
     />
-  </Styles.IconWrapper>
+  </TouchableOpacity>
 );
