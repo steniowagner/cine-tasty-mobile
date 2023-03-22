c28dad37888476527d371c88bfbcc1846f8e163f
Stenio Wagner <stenio.wagner1@gmail.com>
2023-03-22 11:27:11 -0300

    feat: add the animated-header into the FamousDetails screen

--- src/components/screens/common/famous-details/components/FamousDetails.tsx
+++ src/components/screens/common/famous-details/components/FamousDetails.tsx
@@ -1,19 +1,17 @@
-import React, {useLayoutEffect} from 'react';
-import {ScrollView} from 'react-native';
+import React from 'react';
 import {withTheme} from 'styled-components/native';
 
 import {
   ExpansibleTextSection,
   MediaHorizontalList,
   ImagesList,
-  HeaderBackButton,
   Advise,
-  StatusBarStyled,
+  ScrollWithAnimatedHeader,
 } from '@components';
 
 import {FamousDetailStackProps} from '../routes/route-params-types';
 import {HeaderInfo} from './header/header-info/HeaderInfo';
-import useFamousDetail from './useFamousDetails';
+import {useFamousDetail} from './useFamousDetails';
 import * as Styles from './FamousDetails.styles';
 import {DeathDay} from './death-day/DeathDay';
 
@@ -22,72 +20,81 @@ export const FamousDetails = withTheme((props: FamousDetailStackProps) => {
     id: props.route.params.id,
   });
 
-  useLayoutEffect(() => {
-    props.navigation.setOptions({
-      headerLeft: () => (
-        <HeaderBackButton onPress={() => props.navigation.goBack()} />
-      ),
-    });
-  }, [props.navigation]);
-
-  if (famousDetail.hasError) {
-    return (
-      <Advise
-        description={famousDetail.texts.advise.description}
-        suggestion={famousDetail.texts.advise.suggestion}
-        title={famousDetail.texts.advise.title}
-        icon="alert-box"
-      />
-    );
-  }
-
   return (
-    <>
-      <StatusBarStyled />
-      <ScrollView testID="scroll-content">
-        <HeaderInfo
-          knownForDepartment={famousDetail.famous?.knownForDepartment}
-          profileImage={props.route.params.profileImage}
-          placeOfBirth={famousDetail.famous?.placeOfBirth}
-          birthDate={famousDetail.famous?.birthday}
-          isLoading={famousDetail.isLoading}
-          name={props.route.params.name}
+    <ScrollWithAnimatedHeader
+      isLoading={famousDetail.isLoading}
+      hasError={famousDetail.hasError}
+      headerTitle={props.route.params.name}
+      headerBackgroundColorInterpolationInput={
+        famousDetail.animatedHeaderIntepolationParams
+          .headerBackgroundColorInterpolationInput
+      }
+      headerBackgroundColorInterpolationOutput={
+        famousDetail.animatedHeaderIntepolationParams
+          .headerBackgroundColorInterpolationOutput
+      }
+      headerTitleOpacityInterpolationInput={
+        famousDetail.animatedHeaderIntepolationParams
+          .headerTitleOpacityInterpolationInput
+      }
+      headerTitleOpacityInterpolationOutput={
+        famousDetail.animatedHeaderIntepolationParams
+          .headerTitleOpacityInterpolationOutput
+      }>
+      {famousDetail.hasError && (
+        <Advise
+          description={famousDetail.texts.advise.description}
+          suggestion={famousDetail.texts.advise.suggestion}
+          title={famousDetail.texts.advise.title}
+          icon="alert-box"
         />
-        {famousDetail.famous?.deathday && (
-          <DeathDay day={famousDetail.famous.deathday} />
-        )}
-        <Styles.BiographySectionWrapper testID="biography-section">
-          <ExpansibleTextSection
-            sectionTitle={famousDetail.texts.biography}
-            text={famousDetail.famous?.biography}
+      )}
+      {famousDetail.canShowContent && (
+        <>
+          <HeaderInfo
+            knownForDepartment={famousDetail.famous?.knownForDepartment}
+            profileImage={props.route.params.profileImage}
+            placeOfBirth={famousDetail.famous?.placeOfBirth}
+            birthDate={famousDetail.famous?.birthday}
             isLoading={famousDetail.isLoading}
+            name={props.route.params.name}
           />
-        </Styles.BiographySectionWrapper>
-        {famousDetail.famous && (
-          <>
-            {famousDetail.famous.images && (
-              <ImagesList
-                images={famousDetail.famous.images}
-                orientation="PORTRAIT"
-              />
-            )}
-            {famousDetail.famous.moviesCast && (
-              <MediaHorizontalList
-                title={famousDetail.texts.castMoviesSection}
-                dataset={famousDetail.famous.moviesCast}
-                type="MOVIE"
-              />
-            )}
-            {famousDetail.famous.tvCast && (
-              <MediaHorizontalList
-                title={famousDetail.texts.castTvShowSection}
-                dataset={famousDetail.famous.tvCast}
-                type="TV_SHOW"
-              />
-            )}
-          </>
-        )}
-      </ScrollView>
-    </>
+          {famousDetail.famous?.deathday && (
+            <DeathDay day={famousDetail.famous.deathday} />
+          )}
+          <Styles.BiographySectionWrapper testID="biography-section">
+            <ExpansibleTextSection
+              sectionTitle={famousDetail.texts.biography}
+              text={famousDetail.famous?.biography}
+              isLoading={famousDetail.isLoading}
+            />
+          </Styles.BiographySectionWrapper>
+          {famousDetail.famous && (
+            <>
+              {famousDetail.famous.images && (
+                <ImagesList
+                  images={famousDetail.famous.images}
+                  orientation="PORTRAIT"
+                />
+              )}
+              {famousDetail.famous.moviesCast && (
+                <MediaHorizontalList
+                  title={famousDetail.texts.castMoviesSection}
+                  dataset={famousDetail.famous.moviesCast}
+                  type="MOVIE"
+                />
+              )}
+              {famousDetail.famous.tvCast && (
+                <MediaHorizontalList
+                  title={famousDetail.texts.castTvShowSection}
+                  dataset={famousDetail.famous.tvCast}
+                  type="TV_SHOW"
+                />
+              )}
+            </>
+          )}
+        </>
+      )}
+    </ScrollWithAnimatedHeader>
   );
 });
