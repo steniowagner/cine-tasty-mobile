4f37a17c7f2a4268e66958464900900309d29f88
Stenio Wagner <stenio.wagner1@gmail.com>
2023-03-26 16:11:38 -0300

    feat: moving the reviews screen to the screen/common folder

---
+++ src/components/screens/common/reviews/routes/route-params-types.ts
@@ -0,0 +1,31 @@
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RouteProp} from '@react-navigation/native';
+
+import {FamousStackParams} from '@src/components/screens/famous/routes/route-params-types';
+import {HomeStackParams} from '@src/components/screens/home/routes/route-params-types';
+import {checkIsHomeStack} from '@routes/constants';
+import {Routes} from '@routes/routes';
+
+type StackParams = FamousStackParams & HomeStackParams;
+
+type Route = Routes.Famous.MEDIA_REVIEWS | Routes.Home.MEDIA_REVIEWS;
+
+export type Review = {
+  content: string;
+  author: string;
+};
+
+export type ReviewsParams = {
+  mediaTitle: string;
+  reviews: Review[];
+};
+
+export type ReviewsNavigationProps = {
+  navigation: StackNavigationProp<StackParams, Route>;
+  route: RouteProp<StackParams, Route>;
+};
+
+export const getRouteName = (rootParent: string) =>
+  checkIsHomeStack(rootParent)
+    ? Routes.Home.MEDIA_REVIEWS
+    : Routes.Famous.MEDIA_REVIEWS;
