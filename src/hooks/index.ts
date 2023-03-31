b852ed453262173d27a523a4e8bc56e79985ec25
Stenio Wagner <stenio.wagner1@gmail.com>
2023-03-31 15:19:53 -0300

    feat: removing the use-paginated-query hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,4 @@
 export {useGetCurrentISO6391Language} from './useGetCurrentISO6391Language';
-export {usePaginatedQuery} from './use-paginated-query/usePaginatedQuery';
 export {useSystemThemePreference} from './useSystemThemePreference';
 export {useImageFallbackView} from './useImageFallbackView';
 export {useGetCurrentTheme} from './useGetCurrentTheme';
