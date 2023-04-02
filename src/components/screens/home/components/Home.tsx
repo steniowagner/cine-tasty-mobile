8815c1e3f88854bef80380827f3977b50ff63ba0
Stenio Wagner <stenio.wagner1@gmail.com>
2023-04-02 13:29:42 -0300

    feat: resttyling the loading-home

--- src/components/screens/home/components/Home.tsx
+++ src/components/screens/home/components/Home.tsx
@@ -5,7 +5,7 @@ import {PaginatedListHeader} from '@components';
 import * as Types from '@local-types';
 
 import {HomeStackProps} from '../routes/route-params-types';
-import LoadingHome from './top3/loading-top3/LoadingTop3';
+import {LoadingHome} from './loading/LoadingHome';
 import {HomeSection} from './home-section/HomeSection';
 import {Header} from './header/Header';
 import {useHome} from './useHome';
--- src/components/screens/home/components/loading/LoadingHome.styles.ts
+++ src/components/screens/home/components/loading/LoadingHome.styles.ts
@@ -1,28 +1,31 @@
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
 
-import * as Top3ListItemStyles from '../top-3-list-item/Top3ListItem.styles';
+import * as Top3ListItemStyles from '../top3/top-3-list-item/Top3ListItem.styles';
 
 type ItemWrapperStyleProps = {
   isTheMiddle: boolean;
   width: number;
 };
 
-export const RightItemWrapper = styled(View)`
-  margin-left: ${({theme}) => -theme.metrics.getWidthFromDP('67%')}px;
-  margin-top: ${Top3ListItemStyles.ITEM_MARGING_TOP}px;
-  position: absolute;
+export const Wrapper = styled(View)`
+  flex-direction: row;
+  left: -${({theme}) => {
+      const horizontalMargin =
+        theme.metrics.width - Top3ListItemStyles.ITEM_WIDTH;
+      return Top3ListItemStyles.ITEM_WIDTH - horizontalMargin / 2;
+    }}px;
 `;
 
-export const CenterItemWrapper = styled(View)`
-  margin-left: ${({theme}) => theme.metrics.getWidthFromDP('12.5%')}px;
+export const RightItemWrapper = styled(View)`
+  margin-top: ${Top3ListItemStyles.ITEM_MARGING_TOP}px;
+  right: -${({theme}) => theme.metrics.extraLargeSize}px;
 `;
 
 export const LeftItemWrapper = styled(View)`
-  margin-left: ${({theme}) => theme.metrics.getWidthFromDP('92%')}px;
   margin-top: ${Top3ListItemStyles.ITEM_MARGING_TOP}px;
-  position: absolute;
+  left: -${({theme}) => theme.metrics.extraLargeSize}px;
 `;
 
 export const LoadingSmokeShadow = styled(LinearGradient).attrs(({theme}) => ({
@@ -43,7 +46,14 @@ export const SmokeShadow = styled(LinearGradient).attrs(({theme}) => ({
 }))<Pick<ItemWrapperStyleProps, 'isTheMiddle'>>`
   width: ${Top3ListItemStyles.ITEM_WIDTH}px;
   height: ${Top3ListItemStyles.ITEM_HEIGHT + 2}px;
-  margin-horizontal: ${({isTheMiddle, theme}) =>
-    isTheMiddle ? theme.metrics.largeSize : 0}px;
+
   position: absolute;
 `;
+
+export const sheet = StyleSheet.create({
+  loading: {
+    borderRadius: Top3ListItemStyles.ITEM_BORDER_RADIUS,
+    height: Top3ListItemStyles.ITEM_HEIGHT,
+    width: Top3ListItemStyles.ITEM_WIDTH,
+  },
+});
