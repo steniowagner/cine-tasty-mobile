1ca76ca6b9d705ff6c8e6b0b39bde878cd7284f5
Stenio Wagner <stenio.wagner1@gmail.com>
2023-07-14 10:33:03 -0300

    test: creating the SettingsModal

--- src/components/screens/settings/components/settings-modal/settings-modal-option/SettingsModalOption.tsx
+++ src/components/screens/settings/components/settings-modal/settings-modal-option/SettingsModalOption.tsx
@@ -13,14 +13,18 @@ type SettingsOptionProps = {
 
 export const SettingsModalOption = (props: SettingsOptionProps) => (
   <>
-    <Styles.Wrapper onPress={props.onPress}>
+    <Styles.Wrapper
+      onPress={props.onPress}
+      testID="settings-modal-option-button">
       <SVGIcon
         size={metrics.getWidthFromDP(Styles.ICON_WIDTH_PERCENTAGE)}
         style={Styles.iconStyles}
         colorThemeRef="modalTextContent"
         id={props.icon}
       />
-      <Styles.Title>{props.title}</Styles.Title>
+      <Styles.Title testID="settings-modal-option-text">
+        {props.title}
+      </Styles.Title>
     </Styles.Wrapper>
     <Styles.LineDivider />
   </>
