60b7d990a434c3047443b91317c63ba794abe461
Stenio Wagner <stenio.wagner1@gmail.com>
2023-07-21 16:37:11 -0300

    feat: removing unused styles from the SuppotLinks.styles file

--- src/components/screens/home/components/settings/open-source/support-links/SupportLinks.styles.ts
+++ src/components/screens/home/components/settings/open-source/support-links/SupportLinks.styles.ts
@@ -1,18 +1,7 @@
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
 import styled from 'styled-components/native';
-
-type DefaultItemTextStyleProps = {
-  isSubtext?: boolean;
-};
 
 export const SectionWrapper = styled(View)`
   width: 100%;
   margin-bottom: ${({theme}) => theme.metrics.getWidthFromDP('8%')}px;
-`;
-
-export const DefaultItemText = styled(Text)<DefaultItemTextStyleProps>`
-  font-size: ${({theme}) => theme.metrics.largeSize * 1.2}px;
-  color: ${({isSubtext, theme}) =>
-    isSubtext ? theme.colors.subText : theme.colors.text};
-  font-family: CircularStd-Medium;
 `;
