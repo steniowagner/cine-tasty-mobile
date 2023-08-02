95a6d84b98f07ae09642c76ab5361215e8d4e591
Stenio Wagner <stenio.wagner1@gmail.com>
2023-08-02 14:19:31 -0300

    feat: removing unused hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,4 @@
-export {useGetCurrentISO6391Language} from './useGetCurrentISO6391Language';
-export {useSystemThemePreference} from './useSystemThemePreference';
 export {useImageFallbackView} from './useImageFallbackView';
-export {useGetCurrentTheme} from './useGetCurrentTheme';
 export {useTranslations} from './useTranslations';
 export {usePagination} from './pagination/usePagination';
 export {useImperativeQuery} from './useImperativeQuery';
