2b8d76c796fe52de363bea9d3ffdbbf61b5a5d19
Stenio Wagner <stenio.wagner1@gmail.com>
2023-10-07 00:31:09 -0300

    feat: setup eslint and prettier

--- CineTasty/.eslintrc.js
+++ CineTasty/.eslintrc.js
@@ -1,4 +1,33 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'no-console': 1,
+    'no-var': 'error',
+    semi: 'error',
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'no-multi-spaces': 'error',
+    'space-in-parens': 'error',
+    'no-multiple-empty-lines': 'error',
+    'prefer-const': 'error',
+    'object-curly-spacing': [
+      'error',
+      'always',
+      { objectsInObjects: false },
+    ] /*'prettier/prettier': [
+      'error',
+      {
+        bracketSpacing: true,
+        parser: 'flow',
+      },
+    ],*/,
+    '@typescript-eslint/no-unused-vars': [
+      'warn', // or "error"
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+  },
 };
--- CineTasty/App.tsx
+++ CineTasty/App.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -29,7 +29,7 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): JSX.Element {
+function Section({ children, title }: SectionProps): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
