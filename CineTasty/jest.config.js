17ec96e9d6422b4762de7934ff5595df2925c2b5
Stenio Wagner <stenio.wagner1@gmail.com>
2023-10-07 00:09:30 -0300

    feat: setting up jest

--- CineTasty/jest.config.js
+++ CineTasty/jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx', 'node'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.spec.json',
+      babelConfig: true,
+    },
+  },
+  transformIgnorePatterns: [],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 };
