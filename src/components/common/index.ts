3655c218bed8f740d0fc269595dfe2073fbdec87
Stenio Wagner <stenio.wagner1@gmail.com>
2023-10-08 17:49:32 -0300

    feat: creating the SVGIcon component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,10 @@
 import Typography from './typography/Typography';
 
+export {
+  flags,
+  type FlagsIcons,
+  type Icons,
+  SVGIcon,
+  type SVGIconProps,
+} from './svg-icon';
 export { Typography };
---
+++ src/components/common/svg-icon/index.ts
@@ -0,0 +1,3 @@
+export { type SVGIconProps, SVGIcon } from './SVGIcon';
+export { type FlagsIcons, flags } from './flags';
+export { type Icons } from './icons';
