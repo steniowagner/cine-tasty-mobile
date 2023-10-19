f6f42fee9a700c0d6db7269e1dbe685612cc3b76
Stenio Wagner <stenio.wagner1@gmail.com>
2023-10-18 22:29:41 -0300

    feat: creating the PaginatedListFooter

---
+++ src/components/common/paginated-list-footer/PaginatedListFooter.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Platform } from 'react-native';
+
+import metrics from '@styles/metrics';
+
+import * as Styles from './PaginatedListFooter.styles';
+import { SVGIcon } from '../svg-icon/SVGIcon';
+
+type PaginationFooterProps = {
+  onPressReloadButton: () => void;
+  isPaginating: boolean;
+  hasError: boolean;
+};
+
+export const PaginationFooter = (props: PaginationFooterProps) => (
+  <Styles.Wrapper testID="pagination-footer-wrapper">
+    {props.isPaginating && (
+      <Styles.CustomActivityIndicator
+        testID="pagination-loading-footer-wrapper"
+        size={Platform.select({
+          android: 'large',
+          ios: 'small',
+        })}
+      />
+    )}
+    {props.hasError && (
+      <Styles.LoadButton
+        testID="pagination-footer-reload-button"
+        onPress={props.onPressReloadButton}>
+        <SVGIcon size={metrics.getWidthFromDP('10%')} id="restart" />
+      </Styles.LoadButton>
+    )}
+  </Styles.Wrapper>
+);
