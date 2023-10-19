d7017aa57786643d800455845ba1a719fb4a8383
Stenio Wagner <stenio.wagner1@gmail.com>
2023-10-18 23:33:08 -0300

    test: creating the NewsLoading

---
+++ src/components/screens/news/components/news-loading/NewsLoading.test.tsx
@@ -0,0 +1,38 @@
+jest.unmock('react-native-reanimated');
+
+import React from 'react';
+import { RenderAPI, cleanup, render } from '@testing-library/react-native';
+import { ThemeProvider } from 'styled-components/native';
+
+import { dark as theme } from '@styles/themes/dark';
+
+import { NewsLoading, INITIAL_ITEMS_TO_RENDER } from './NewsLoading';
+
+const renderNewsLoading = () => (
+  <ThemeProvider theme={theme}>
+    <NewsLoading />
+  </ThemeProvider>
+);
+
+describe('Screens/News/NewsLoading', () => {
+  afterEach(cleanup);
+
+  const elements = {
+    newsLoadingList: (api: RenderAPI) => api.queryByTestId('news-loading-list'),
+    newsListItems: (api: RenderAPI) =>
+      api.queryAllByTestId('news-loading-list-item'),
+  };
+
+  it('should render the component correctly', () => {
+    const component = render(renderNewsLoading());
+    expect(elements.newsLoadingList(component)).not.toBeNull();
+    expect(elements.newsListItems(component)).not.toBeNull();
+  });
+
+  it('should render the correct number of items', async () => {
+    const component = render(renderNewsLoading());
+    expect(elements.newsListItems(component).length).toEqual(
+      INITIAL_ITEMS_TO_RENDER,
+    );
+  });
+});
