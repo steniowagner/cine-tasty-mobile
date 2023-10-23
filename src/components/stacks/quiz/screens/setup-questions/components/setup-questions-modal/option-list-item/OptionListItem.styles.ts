2202a56b0268faa767fae418524ee6ce20091a75
Stenio Wagner <stenio.wagner1@gmail.com>
2023-10-23 13:21:58 -0300

    feat: creating the OptionListItem

---
+++ src/components/stacks/quiz/screens/setup-questions/components/setup-questions-modal/option-list-item/OptionListItem.styles.ts
@@ -0,0 +1,34 @@
+import { TouchableOpacityProps } from 'react-native';
+import styled, { IStyledComponent } from 'styled-components/native';
+import { Substitute } from 'styled-components/native/dist/types';
+
+import { Typography, SVGIcon } from '@common-components';
+import { dark } from '@styles/themes';
+
+type ListItemStyleProps = {
+  isSelected: boolean;
+};
+
+export const ListItemWrapper: IStyledComponent<
+  'native',
+  Substitute<TouchableOpacityProps, ListItemStyleProps>
+> = styled.TouchableOpacity<ListItemStyleProps>`
+  width: 100%;
+  height: ${({ theme }) => theme.metrics.getWidthFromDP('20')}px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-horizontal: ${({ theme }) => theme.metrics.lg}px;
+  background-color: ${({ isSelected }) =>
+    isSelected ? dark.colors.background : dark.colors.text};
+`;
+
+export const ListItemText = styled(
+  Typography.SmallText,
+).attrs<ListItemStyleProps>(({ isSelected }) => ({
+  color: isSelected ? dark.colors.text : dark.colors.buttonText,
+}))``;
+
+export const CheckboxIcon = styled(SVGIcon).attrs(({ theme }) => ({
+  size: theme.metrics.getWidthFromDP('8'),
+}))``;
