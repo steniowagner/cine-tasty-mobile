f9e812a65b9d90c7c49eeb30d330a7e11704a295
Stenio Wagner <stenio.wagner1@gmail.com>
2023-11-12 22:42:30 -0300

    feat: creating the use-tmdb-image-uri

---
+++ src/hooks/use-tmdb-image-uri/use-tmdb-image-uri.ts
@@ -0,0 +1,33 @@
+import { useCallback } from 'react';
+
+import { ImageType } from '@/providers/tmdb-image-qualities/types';
+import { useTMDBImageQualities } from '@providers';
+
+export const BASE_URL = 'https://image.tmdb.org/t/p';
+export const THUMBNAIL_URL = `${BASE_URL}/w45`;
+
+type GetURIParams = {
+  isThumbnail?: boolean;
+  imageType: ImageType;
+  image: string;
+};
+
+export const useTMDBImageURI = () => {
+  const tmdbImagesQualities = useTMDBImageQualities();
+
+  const uri = useCallback(
+    (params: GetURIParams) => {
+      if (params.isThumbnail) {
+        return `${THUMBNAIL_URL}${params.image}`;
+      }
+      if (tmdbImagesQualities.mappingImageTypeToImageSize) {
+        return `${BASE_URL}/${
+          tmdbImagesQualities.mappingImageTypeToImageSize[params.imageType]
+        }${params.image}`;
+      }
+    },
+    [tmdbImagesQualities.mappingImageTypeToImageSize],
+  );
+
+  return { uri };
+};
